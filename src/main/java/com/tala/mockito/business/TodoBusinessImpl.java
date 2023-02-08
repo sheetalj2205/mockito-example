467aa6fde11403d5d289fb9f2fee935264c94886
sheetalj2205 <sheetalj2205@gmail.com>
2023-02-08 14:13:52 +0530

    Feat: Added class which needs to be tested and it depends on the class written by some other team

--- src/main/java/com/tala/mockito/business/TodoBusinessImpl.java
+++ src/main/java/com/tala/mockito/business/TodoBusinessImpl.java
@@ -1,2 +1,27 @@
-package com.tala.mockito.business;public class TodoBusinessImpl {
+package com.tala.mockito.business;
+
+import com.tala.mockito.data.api.TodoService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//TodoService dependency
+public class TodoBusinessImpl {
+    private TodoService todoService;
+
+    public TodoBusinessImpl(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    public List<String> retrieveTodosRelatedToSpring(String user) {
+        List<String> filteredTodos = new ArrayList<String>();
+        List<String> todos = todoService.retrieveTodos(user);
+
+        for (String todo : todos) {
+            if (todo.contains("Spring")) {
+                filteredTodos.add(todo);
+            }
+        }
+        return filteredTodos;
+    }
 }
