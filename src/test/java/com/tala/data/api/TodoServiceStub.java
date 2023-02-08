b4d12eaf01ae82b22633072ae42f21ee1c11a406
sheetalj2205 <sheetalj2205@gmail.com>
2023-02-08 14:14:35 +0530

    Feat: Class added to test my class

--- src/test/java/com/tala/data/api/TodoServiceStub.java
+++ src/test/java/com/tala/data/api/TodoServiceStub.java
@@ -1,2 +1,13 @@
-package com.tala.data.api;public class TodoServiceStub {
+package com.tala.data.api;
+
+import com.tala.mockito.data.api.TodoService;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TodoServiceStub implements TodoService {
+    public List<String> retrieveTodos(String User){
+        return Arrays.asList("welcome to tala", "learn mockito");
+    }
 }
